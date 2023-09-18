1dfebf09f7420c14119f4256bd9923687f6c5ab3
Ian Spresney <41807694+ItsSpres@users.noreply.github.com>
2023-09-18 14:50:21 -0700

    added bank account proj

---
+++ CSD205 Python/BankAccountProject/BankAccount.py
@@ -0,0 +1,13 @@
+
+class BankAccount:
+    def __init__(self, checking, savings):
+        print('nothing yet')
+
+
+# Sub class for bank account
+class Checking(BankAccount):
+
+
+# Sub class for bank account 
+class Savings(BankAccount):
+
