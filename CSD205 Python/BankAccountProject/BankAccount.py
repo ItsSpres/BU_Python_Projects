e7cbacf6076640765c6e1c3afa32737fb1d2e4f0
Ian Spresney <41807694+ItsSpres@users.noreply.github.com>
2023-09-18 21:33:14 -0700

    added more code to bank account assignment

--- CSD205 Python/BankAccountProject/BankAccount.py
+++ CSD205 Python/BankAccountProject/BankAccount.py
@@ -1,13 +1,60 @@
 
-class BankAccount:
-    def __init__(self, checking, savings):
-        print('nothing yet')
 
+# This is the parent class
+class BankAccount:
+
+
+    def __init__(self, accountNumber, balance):
+        self.accountNumber = accountNumber
+        self.balance = balance
+
+    def withdrawal(self, amount):
+        # Checking if user has enough money in account
+        if amount <= self.balance:
+            # Subtracting the money from the balance
+            self.balance -= amount
+            print('Your withdrawal went through.')
+        else:
+            print('Not enough funds!')
+    
+    # Whenever there is a deposit; take the amount and add to balance.
+    def deposit(self, amount):
+        self.balance += amount
+        print('Yay! You added money.')
+
+    # Simply returns the users account balance for the instnace.
+    def getBalance(self):
+        return self.balance
 
 # Sub class for bank account
 class Checking(BankAccount):
 
+    def __init__(self, accountNumber, balance, fees, min_balance):
+        super().__init__(accountNumber, balance)
+        self.fees = fees
+        self.min_balance = min_balance
+
+    # deducting fees from the users account.
+    def deductFees(self):
+        print('')
+
+    # Checking if the minimum balance is met
+    # if not then we must notify the user. 
+    def checkMinimumBalance(self):
+        print('')
 
 # Sub class for bank account 
 class Savings(BankAccount):
+    
+    def __init__(self, accountNumber, balance, interest_rate):
+        super().__init__(accountNumber, balance)
+        self.interest_rate = interest_rate
 
+    # Adding interest to the savings account.
+    def addInterest(self):
+        return self.balance * self.interest_rate
+        
+    
+
+
+# Program
