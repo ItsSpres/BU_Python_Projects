37963e167de8c8b54e1b65e3c24c8da6ad52d3a8
Ian Spresney <itsians@icloud.com>
2023-09-19 08:39:37 -0700

    added more documentation

--- CSD205 Python/BankAccountProject/BankAccount.py
+++ CSD205 Python/BankAccountProject/BankAccount.py
@@ -1,3 +1,12 @@
+# Ian Spresney
+# CSD205 - Python
+# Module 9 Assignment
+# 09/19/2023
+# GitHub Repo: https://github.com/ItsSpres/My_University_Projects_BU/blob/main/CSD205%20Python/BankAccountProject/BankAccount.py
+# Sources that helped me to complete this assignment:
+# https://www.w3schools.com/python/
+# The provided module 9 video tutorials. 
+# Some previous knowledge of mine was also used for this assignment as I have taken other Python classes before.
 
 
 # This is the parent class
@@ -59,5 +68,59 @@ class Savings(BankAccount):
         
     
 
+# For the project we will be useing the following and have to go through the program twice.
+# Balance: $200, $25
+# Fees: $5
+# Min Balance: $50
+# Interest Rate: 2%
+# For account numbers we can just make things up. 
+# There are included examples for withdrawal and deposit. I have commented these out for now.
 
-# Program
+# Getting user input and checking for valid inputs.
+while True:
+    try:
+        # asking the user for the specific inputs.
+        account_type = input("Enter account type (C for Checking or S for Savings): ").capitalize()
+        account_number = input("Enter account number: ")
+        balance = float(input("Enter your initial balance: "))
+
+        # if a certain condition is met then proceed. 
+        # in this case it is having to do with the checking and savings accounts
+        if account_type == 'C':
+            fees = 5
+            min_balance = 50
+            account = Checking(account_number, balance, fees, min_balance)
+        elif account_type == 'S':
+            interest_rate = 0.02
+            account = Savings(account_number, balance, interest_rate)
+        else:
+            print("Invalid account type. Please enter 'C' or 'S'.")
+
+        # These are only examples for fun if we would like to simulate a deposit or withdrawal.
+        # account.withdrawal(10)  
+        # account.deposit(100)    
+
+        # Now we will handle the logic for subtracting fees, checking the minimum balnce, and adding interest.
+        # First we need to check the account instance that we are in. 
+        if isinstance(account, Checking):
+            account.deductFees()
+            account.checkMinimumBalance()
+        elif isinstance(account, Savings):
+            interest_earned = account.addInterest()
+            print(f'Interest earned: ${interest_earned:.2f}')
+
+    # handling different types of errors that may occur.
+    except ValueError:
+        print("Not a valid input.")
+    except Exception as error:
+        print(f"An error occurred: {error}")
+
+    # Asking the user if they want to continue.
+    # For this assignment we must go through the program twice.
+    try:
+        again = input("Do you want to continue? (y or n): ").lower()
+        if again != 'y':
+            break # if the user does not type 'y' then the program will terminate. 
+    # More error handling for this specific try and input operation. 
+    except Exception as error:
+        print(f"An error occurred: {error}")
