984f3aa033dc70596fd29b56576cf492e47f9198
Ian Spresney <itsians@icloud.com>
2023-09-27 22:10:18 -0700

    updated

--- CSD205 Python/FileApp/main.py
+++ CSD205 Python/FileApp/main.py
@@ -3,29 +3,43 @@
 # 09-27-2023
 # Sources:
 # https://www.geeksforgeeks.org/python-list-files-in-a-directory/
+# https://docs.python.org/3/library/dialog.html#:~:text=tkinter.-,filedialog%20—%20File%20selection%20dialogs,creating%20file%2Fdirectory%20selection%20windows.
+
+# I started looking into printing all the directories to the terminal which seemed like way too much to do
+# I did it and then decided I could number all of them and then let the user select them. 
+# However, I felt like I was reinventing the wheel or rather the terminal. 
+# As an ls and cd command can allow a user to navigate with ease. 
+# Since the assignment wasn't too specific on what I can and cannot use.
+# I decided to use the tkinter module. 
+# I have used this before for assignments in my CIS classes, specifically my python weather app. 
+# This is a much simpler and straight forward method. Less coding too. Yay!
+# Essentially giving the user a much better user expereince. Which is what we want, right? 
+
 
 # A module for interacting with the operating system. 
 import os
+# The module we will use for showing the user our directories and for selecting them.
 import tkinter as tk
 from tkinter import filedialog
 
-# Create the root window
-root = tk.Tk()
-root.withdraw()
+while True:
 
-# Ask the user to select a directory
-selected_directory = filedialog.askdirectory(title="Select Directory")
+    # Making the root window with tkinter
+    rt = tk.Tk()
+    rt.withdraw()
 
-# Check if the user selected a directory
-if selected_directory:
-    print(f"Selected directory: {selected_directory}")
+    # pop directories and allow user to make a selection.
+    selectedDir = filedialog.askdirectory(title="Select Directory")
 
-    # Get the list of entries (files and subdirectories) in the selected directory
-    entries = os.listdir(selected_directory)
+    # checking that a directory was actually selected. 
+    if selectedDir:
 
-    # Print the entries
-    for entry in entries:
-        print(entry)
-else:
-    print("No directory selected.")
+        print(f"You have selected \033[4m{selectedDir}\033[0m as your file save location.")
+        break # exiting loop
 
+     else: 
+
+        print("An error has occured or no directory was selected.")
+
+def create_file():
+    print("hello world")
