bab1fb6c137fbc9194f9fa8d81c370ced856079c
Ian Spresney <itsians@icloud.com>
2023-10-02 01:32:28 -0700

    fixed python app for module 12

--- CSD205 Python/DictionaryProject/ispresney-pythonDict.py
+++ CSD205 Python/DictionaryProject/ispresney-pythonDict.py
@@ -1,7 +1,6 @@
 #Ian Spresney 
 
-def main():
-    pcHardware = {
+pcHardware = {
         "CPU": "9700K",
         "GPU": "RTX 4070",
         "CPUSpeed": "5.0Ghz",
@@ -14,26 +13,43 @@ def main():
         "CPUCoolerStyle": "Water",
     }
 
+def main():
     print("Welcome to the PC Hardware!")
     print("Here are some of the items you may view.")
     for hardware in pcHardware:
         print("- " + hardware)
-    
-
-    def getAndDisplayHardware():
-        selected_item = input("Select an item to view more about it: ")
-
-        if selected_item in pcHardware:
-            detail = pcHardware[selected_item]
-            print(f"{selected_item} is/are: {detail}")
-            getAndDisplayHardware()
-        else:
-            print("Sorry, that item is not in our dictionary.")
-            getAndDisplayHardware()
-
     getAndDisplayHardware()
 
-    #Add quit/exit function maybe???
+
+def getAndDisplayHardware():
+    selected_item = input("Select an item to view more about it: ")
+
+    if selected_item in pcHardware:
+        detail = pcHardware[selected_item]
+        print(f"{selected_item} is/are: {detail}")
+        continueOrEnd()
+        # getAndDisplayHardware()
+    else:
+        print("Sorry, that item is not in our dictionary.")
+        continueOrEnd()
+        # getAndDisplayHardware()
+
+def continueOrEnd():
+    userInput = input("Do you wish to continue (y or n): ")
+   
+    match userInput:
+        case "y":
+            print("Restarting...")
+            main()
+            getAndDisplayHardware()
+
+        case "n":
+            print("...the program has ended...")
+           
+        case default:
+            print("...your input was invalid...")
+            print("Program ended...")
+            
 
 #Program Start
 main()
