43e6d34a5a97c3fb3136896ab9ad9a518e99e6f6
Ian Spresney <41807694+ItsSpres@users.noreply.github.com>
2023-11-26 13:31:23 -0800

    project done

--- CSD320 Java/Password/Password.java
+++ CSD320 Java/Password/Password.java
@@ -5,8 +5,15 @@
  Module 7 Assignment
 
 Sources:
-https://www3.ntu.edu.sg/home/ehchua/programming/howto/Regexe.html
+- https://www3.ntu.edu.sg/home/ehchua/programming/howto/Regexe.html
+- Other information used from previous assignments like 'Scanner'.
+- https://stackoverflow.com/questions/13674449/checking-password-code
  */
+
+//NOTE!!!
+//the program will prompt user to type password again if typed incorrectly...
+//when password is valid, then program will end...
+
 
 //SOURCE - https://www3.ntu.edu.sg/home/ehchua/programming/howto/Regexe.html
 import java.util.regex.Matcher;
@@ -34,6 +41,21 @@ public class Password {
                 System.out.println("Invalid password. Please make sure it meets the specified criteria.");
             }
         }
+
+        //this is the code for the program to end when password is right or wrong.
+        //NO LOOP, but I think the loop is a nice touch.
+        /*
+            // Ask the user for password input
+            System.out.print("Enter your password: ");
+            String passwordInput = scanner.nextLine();
+
+            // Validate the password
+            if (isValidPassword(passwordInput)) {
+                System.out.println("Valid password!");
+            } else {
+                System.out.println("Invalid password. Please make sure it meets the specified criteria.");
+            }
+        */
 
         // Close the scanner
         scanner.close();
